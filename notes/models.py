59f62571f4b5f96b4b40d6a0d55a72c402b3aa0b
gonchigor <gonchigor@tut.by>
2019-09-02 19:45:16 +0300

    create model Note

--- notes/models.py
+++ notes/models.py
@@ -10,3 +10,14 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
 
+
+class Note(models.Model):
+    category = models.ForeignKey(Category, models.PROTECT, related_query_name="category", related_name="category")
+    name = models.CharField("Наименование", max_length=50, unique=True)
+    header = models.CharField("Заголовок", max_length=100)
+    text = models.TextField("Текст заметки")
+    password = models.CharField("Пароль", max_length=250)
+
+    class Meta:
+        verbose_name = 'заметка'
+        verbose_name_plural = 'заметки'
