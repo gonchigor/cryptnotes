db442002f16f97afdc91593cfb2c26c41dca627b
gonchigor <gonchigor@tut.by>
2019-09-03 01:31:50 +0300

    add crispy form

--- encnotes/settings.py
+++ encnotes/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ] + [
-    'notes'
+    'notes',
 ]
 
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
