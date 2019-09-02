83c2825626edd5b01c0c53ccc5f6f7e1921245f1
gonchigor <gonchigor@tut.by>
2019-09-03 01:24:14 +0300

    add createview for category

--- notes/urls.py
+++ notes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, CategoryCreateView
 
 app_name = 'notes'
 urlpatterns = [
     path('', CategoryListView.as_view(), name='category-list'),
+    path('category/create', CategoryCreateView.as_view(), name='category-create'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Category, Note
+from .form import CategoryForm
+from django.urls import reverse_lazy
 # Create your views here.
 
 
 class CategoryListView(ListView):
     model = Category
+
+
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    success_url = reverse_lazy('notes:category-list')
