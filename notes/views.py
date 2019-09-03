0574e0a39065a89957299de9f52874bce42ce955
gonchigor <gonchigor@tut.by>
2019-09-03 08:32:39 +0300

    done create view for note

--- notes/views.py
+++ notes/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from .models import Category, Note
 from .form import CategoryForm, NoteForm
 from django.urls import reverse_lazy
+from django.http.response import HttpResponseRedirect
+from .crypto import encrypt_message, password_hasher
 # Create your views here.
 
 
@@ -35,4 +37,14 @@ class NoteCreateView(CreateView):
         initial = super().get_initial()
         initial['category'] = self.kwargs['pk']
         return initial
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.text = encrypt_message(self.object.text, self.object.password)
+        self.object.password = password_hasher(self.object.password)
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
+
+    def get_success_url(self):
+        return reverse_lazy('notes:category-detail', kwargs={'pk': self.kwargs['pk']})
 
